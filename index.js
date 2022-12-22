34855729c6168ed69e40988f5303d8050f4bef92
Reza Iqronoor <rezaiqronoor.dev@gmail.com>
2022-12-22 18:56:19 +0700

    add title, set grid and erase button

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ container.addEventListener("mousedown", () => {
    draw = true;
 });
 container.addEventListener("mouseup", () => {
-   draw = false
+   draw = false;
 });
+
 
 for (i = 0; i < columns; i++) {
    const column = document.createElement("div");
@@ -22,14 +23,51 @@ for (i = 0; i < columns; i++) {
       const row = document.createElement("div");
       row.setAttribute("class", "row");
       row.setAttribute("draggable", "false");
-      row.addEventListener("mousemove", () =>
-         {
-            if(draw) row.setAttribute("style", "background-color: blue");
-         }
-      );
+      row.addEventListener("mousemove", () => {
+         if (draw) row.setAttribute("style", "background-color: black");
+      });
 
       column.appendChild(row);
    }
 
    container.appendChild(column);
 }
+
+// Buttons
+
+const btnSetGrid = document.querySelector(".btn-set-grid");
+btnSetGrid.addEventListener("click", () => setGrid());
+
+const setGrid = () => {
+   const gridAmount = +prompt("Please enter the amount of grid you want to set, from 3-100.");
+
+   if(gridAmount < 3 || gridAmount > 100) alert("Please enter the number in range of 3-100");
+   else {
+      document.querySelectorAll('.column').forEach(e => e.remove());
+      document.querySelectorAll('.row').forEach(e => e.remove());
+
+      for (i = 0; i < gridAmount; i++) {
+         const column = document.createElement("div");
+         column.setAttribute("class", "column");
+         column.setAttribute("draggable", "false");
+      
+         for (j = 0; j < gridAmount; j++) {
+            const row = document.createElement("div");
+            row.setAttribute("class", "row");
+            row.setAttribute("draggable", "false");
+            row.addEventListener("mousemove", () => {
+               if (draw) row.setAttribute("style", "background-color: black");
+            });
+      
+            column.appendChild(row);
+         }
+      
+         container.appendChild(column);
+      }
+   };
+}
+
+const btnErase = document.querySelector(".btn-erase");
+btnErase.addEventListener("click", () => {
+   document.querySelectorAll(".row").forEach(e => e.setAttribute("style", "background-color: lightgrey"));
+});
